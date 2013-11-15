6f61003e650a64c60f227d2d5e6322252e0ac1dd
moriarty <alexander@dal.ca>
2013-11-15 14:50:13 +0100

    moved MplCanvas to it's own file in ompl_gui package

---
+++ MplCanvas.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+"""
+# Title: MplCanvas Superclass
+# Description: MplCanvas is a FigureCanvas to display matplotlib plots in Qt
+# Author: @moriarty, @amdshameer
+# Licence: 
+"""
+
+# Import statements
+from PyQt4 import QtCore, QtGui
+import matplotlib
+from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+
+## MplCanvas and MyStaticMplCanvas from embedding_in_qt4 tutorial on matplotlib site.
+class MplCanvas(FigureCanvas):
+    """Ultimately, this is a QWidget (as well as a FigureCanvasAgg, etc.)."""
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+        # We want the axes cleared every time plot() is called
+        self.axes.hold(False)
+
+        self.compute_initial_figure()
+
+        FigureCanvas.__init__(self, fig)
+        self.setParent(parent)
+
+        FigureCanvas.setSizePolicy(self,
+                                   QtGui.QSizePolicy.Expanding,
+                                   QtGui.QSizePolicy.Expanding)
+        FigureCanvas.updateGeometry(self)
+
+    def compute_initial_figure(self):
+        pass
+
+    def clear(self):
+        self.axes.clear()
+        self.draw()
