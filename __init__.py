3c514688ba8fb81686e6d475bdfb17d141add542
moriarty <alexander@dal.ca>
2013-11-15 21:27:15 +0100

    Added imports to ompl_gui __init__.py
    Import all classes from there files. 
    Now instead of 
    ompl_gui.BoundsBox.BoundsBox()
    just
    ompl_gui.BoundsBox()

---
+++ __init__.py
@@ -0,0 +1,20 @@
+"""
+ompl_gui package
+
+A package containing classes related to a simple Open Motion Planning Library GUI
+"""
+__all__=['BoundsBox.py', 'MplCanvas', 'MyStaticMplCanvas', 
+         'Pose2DBox.py', 'SolveWidget', 'BoundsWidget', 
+         'MainWidget', 'MPLViewer', 'PlannersWidget', 
+         'ProblemWidget']
+
+from BoundsBox import BoundsBox
+from MplCanvas import MplCanvas
+from MyStaticMplCanvas import MyStaticMplCanvas
+from Pose2DBox import Pose2DBox
+from SolveWidget import SolveWidget
+from BoundsWidget import BoundsWidget
+# TODO from MainWidget import MainWidget
+from MPLViewer import MPLViewer
+# TODO from PlannersWidget import PlannersWidget
+# TODO from ProblemWidget import ProblemWidget
