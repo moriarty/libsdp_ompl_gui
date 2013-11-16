d0288b029646a924b66c71a512300bcdfb38b744
moriarty <alexander@dal.ca>
2013-11-16 18:17:03 +0100

    Added OptionBox
    Box to group Planner options together
    
    TODO: connect setDefaults to Reset button in solve widget
    TODO: discuss restructuring package to put boxes into subpackages.

---
+++ OptionBox.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+"""
+# Title:OptionBox
+# Description: QGroupBox for holding planner options
+# Author: @moriarty, @amdshameer
+# Licence: 
+"""
+# import statement
+from PyQt4 import QtCore, QtGui
+from PyQt4.QtCore import pyqtSignal as Signal
+
+class OptionBox(QtGui.QGroupBox):
+    """docstring for OptionBox"""
+    def __init__(self, title):
+        super(OptionBox, self).__init__(title)
+        
+        goalBiasLabel = QtGui.QLabel('Goal bias')
+        self.goalBias = QtGui.QDoubleSpinBox()
+        self.goalBias.setRange(0, 1)
+        self.goalBias.setSingleStep(0.05)
+        self.goalBias.setValue(0.05)
+
+        layout = QtGui.QGridLayout()
+        layout.addWidget(goalBiasLabel, 0, 0, QtCore.Qt.AlignRight)
+        layout.addWidget(self.goalBias, 0, 1, 1, 2)
+        self.setLayout(layout)
+
+    def setGoalBias(self, value=0.05):
+        self.goalBias.setValue(value)
+
+    def setDefaults(self):
+        self.setGoalBias()
+
