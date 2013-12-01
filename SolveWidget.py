9553dfc5955da249e47c58c6e9950620e9f04dc6
moriarty <alexander@dal.ca>
2013-12-01 19:09:39 +0100

    Added button to clear plans list
    Multi plots #28 requires a way to clear the list of found paths.
    The button is disabled by default, like #29 suggests for plot button.
    
    The button needs to be connected

--- SolveWidget.py
+++ SolveWidget.py
@@ -18,18 +18,24 @@ class SolveWidget(QtGui.QWidget):
         super(SolveWidget, self).__init__()
         self.solveButton = QtGui.QPushButton('Solve')
         self.plotButton = QtGui.QPushButton('Plot')
-        self.plotButton.setEnabled(False)
-
+        self.removePaths = QtGui.QPushButton('Clear Plans')
         self.clearButton = QtGui.QPushButton('Clear Plot')
         self.resetButton = QtGui.QPushButton('Reset Settings')
+
+        self.enablePlotButton(False)
+        self.enablePlansButton(False)
 
         layout = QtGui.QGridLayout()
         layout.addWidget(self.solveButton, 0, 0)
         layout.addWidget(self.plotButton, 0, 1)
-        layout.addWidget(self.clearButton, 0, 2)
-        layout.addWidget(self.resetButton, 0, 3)
+        layout.addWidget(self.removePaths, 0, 2)
+        layout.addWidget(self.clearButton, 0, 3)
+        layout.addWidget(self.resetButton, 0, 4)
         self.setLayout(layout)
 
     def enablePlotButton(self, setEnabled=True):
         self.plotButton.setEnabled(setEnabled)
+
+    def enablePlansButton(self, setEnabled=True):
+        self.removePaths.setEnabled(setEnabled)
 
